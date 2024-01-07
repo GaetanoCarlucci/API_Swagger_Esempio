48c16838c734c37cd8df3123ae46d1d2131ef011
Gaetano Carlucci <gaetano.carlucci@gmail.com>
2024-01-07 22:29:37 +0100

    int 2

---
+++ interrogazione_2.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const app = express();
+const port = 3000; // Porta in ascolto del server
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Domanda interrogazione 2',
+      version: '1.0.0',
+      contact: {
+        name: 'interrogazione_2',
+        email: 'gaetano.carlucci.sg28391@iissvoltadegemmis.edu.it',
+        url: 'https://github.com/GaetanoCarlucci/API_Swagger_Esempio',
+      },
+    },
+  },
+  apis: ['interrogazione_2.js'], // Sostituisci con il nome del tuo file principale
+};
+
+const swaggerSpec = swaggerJsDoc(options);
+
+app.use('/documentazione', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+
+app.use(express.text());
+
+// Endpoint per la somma con parametri nell'URL
+/**
+ * @openapi
+ * ??path??:
+ *   ??method??:
+ *     summary: Esegue la somma di XX numeri.
+ *     description: Questo endpoint accetta tre parametri numerici (num1, num2 e num3) nell'URL e restituisce la loro somma.
+ *     parameters:
+ *       - in: query
+ *         name: num1
+ *         required: XXX
+ *         schema:
+ *           type: XXX
+ *         description: Il primo numero da sommare.
+ *       - in: query
+ *         name: num2
+ *         required: XXX
+ *         schema:
+ *           type: XXX
+ *         description: Il secondo numero da sommare.
+ *     responses:
+ *       '200':
+ *         description: Successo. Restituisce la somma dei tre numeri.
+ *         content:
+ *           XXX/YYY:
+ *             schema:
+ *               type: XXX
+ *               example: 12
+ *       '400':
+ *         description: Errore nei parametri della richiesta.
+ *         content:
+ *           XXX/YYY:
+ *             schema:
+ *               type: XXX
+ *               example: I parametri della somma non sono numeri validi.
+ *     tags:
+ *       - Modificare qui
+ */
+app.get('/api/somma', (req, res) => {
+  // Estrai i parametri num1, num2 e num3 dalla query dell'URL
+  var num1 = Number(req.query.num1);
+  var num2 = Number(req.query.num2);
+  var num3 = Number(req.query.num3);
+
+  if (!('num1' in req.query)) {
+    res.status(412).send('Manca almeno un parametro richiesto');
+    return;
+  }
+
+  // Verifica se i parametri sono numeri validi
+  if (isNaN(num1) || isNaN(num2)) {
+    res.status(400).send('I parametri della somma non sono numeri validi.');
+  } else {
+    var result = num1 + num2;
+    res.send(result.toString());
+  }
+});
+
+// Avvia il server su localhost
+app.listen(port, 'localhost', () => {
+    console.log(`Server in ascolto sulla porta ${port}`);
+});
